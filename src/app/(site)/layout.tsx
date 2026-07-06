479e589ccc0488dcc46d6a5f1a9868f5882e1d9d
gabrielabattalini <gabrielbattagliniadvocacia@gmail.com>
2026-07-06 05:35:58 +0000

    Site: icones reais das redes sociais e botao flutuante do WhatsApp

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import { Mulish, Spectral } from "next/font/google";
 import "@/components/site/site.css";
 import { CookieBanner } from "@/components/site/CookieBanner";
+import { WhatsAppBtn } from "@/components/site/WhatsAppBtn";
 
 const mulish = Mulish({
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export default function SiteLayout({ children }: { children: React.ReactNode }) 
   return (
     <div className={`${mulish.variable} ${spectral.variable}`}>
       {children}
+      <WhatsAppBtn />
       <CookieBanner />
     </div>
   );
